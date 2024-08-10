781f8688b4a29ebb157d8115baf0b77a0da188a8
lkereceni <lovro.kereceni@gmail.com>
2024-08-10 10:46:07 +0200

    Initial commit

--- src/api/index.ts
+++
@@ -1,6 +0,0 @@
-export const getCurrentWeather = async () => {
-  const response = await fetch(
-    "https://api.openweathermap.org/data/2.5/weather?q=kalinovac&units=metric&appid=961dd78fd92acfcb9627752c4105a471"
-  );
-  return response.json();
-};
